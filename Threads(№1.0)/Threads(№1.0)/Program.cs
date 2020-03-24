497fc24db59d6d38aeb86d3a10b62a80b66a25ff
Bulat Shavaliev <shavaliev-bulat@mail.ru>
2020-03-24 19:15:23 +0400

    первое изменение

---
+++ Threads(№1.0)/Threads(№1.0)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Threads__1._0_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
